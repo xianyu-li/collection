30824e6bdfff962b3741c4d7fa61adbcaf18075f
Xianyu Li <lix113@newschool.edu>
2019-12-16 17:34:12 -0500

    initial push

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // console.log('--js loaded--')
+
+const filters = document.getElementById('filters').getElementsByTagName('a')
+const images = document.getElementById('images').getElementsByTagName('li')
+
+for (let i = 0; i < filters.length; i++){
+    const filter = filters [i];
+    filter.addEventListener('click', function(event){
+        console.log('click', event.target.getAttribute('data-filter')
+        event.preventDefault();
+        filterImages(event.target.getAttribute('data-filter'));
+    }
+
+    );
+};
+
+filterImages = function(filterClass){
+    for (let i =0; i<images.length; i++){
+        if(!images[i].classList.contains(filterClass)){
+            images[i].classList.add('hide');
+        } else{
+            images[i].classList.remove('hide');
+        }
+    }
+}
